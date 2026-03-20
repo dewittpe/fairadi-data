67d0f286206c078418e546a193f9678488f26949
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2026-03-20 09:42:54 -0600

    add ADI/adi_utilities.R and ADI/topic01.R

---
+++ ADI/adi_utilities.R
@@ -0,0 +1,106 @@
+################################################################################
+# file: adi_utilities.R
+#
+# Objective: define several utilities to simplify the building of the ADI
+#
+# Methods defined in this file:
+#
+#   import_census_table
+#   check_for_anotations
+#
+# Variables defined in this file
+#
+#   COLS_TO_KEEP
+#
+################################################################################
+
+################################################################################
+#                             Import census table
+#
+# Function to import all the yearly files for a specific variable
+#
+# @param x the table to import
+# @param ... passed to data.table::fread
+#
+# @example
+#
+import_census_table <- function(table, ...) {
+  stopifnot(!is.null(table), is.character(table), length(table) == 1L)
+  if (startsWith(table, "P")) {
+    path <- "Decennial"
+  } else if (startsWith(table, "B") | startsWith(table, "C")) {
+    path <- "ACS5"
+  } else {
+    stop("Expected table name to start with a 'P' for Decennial table, 'B' or 'C' for ACS5 table.")
+  }
+  path <- file.path("..", path)
+
+  files <-
+    list.files(
+      path = path,
+      pattern = sprintf("%s__\\d{4}\\.csv\\.gz$", table),
+      full.names = TRUE
+    )
+
+  message(paste("Importing data from:\n ", paste(files, collapse = "\n  "), "\n"))
+
+  DTs <- lapply(files, data.table::fread, na.strings = c("NA", "null"), ...)
+  data.table::rbindlist(DTs)
+}
+
+################################################################################
+#                            Check for Annotations
+#
+# It is critically important to look for columns appended with EA "Estimate
+# Anotation" or MA for "Margin of Error Annotation."  These columns will
+# indicate that the numeric value reported in the respected E or M column is not
+# to be treated as a numeric value.
+#
+# Notes on ACS Estimate and Annotation Values
+# (https://www.census.gov/data/developers/data-sets/acs-1year/notes-on-acs-estimate-and-annotation-values.html)
+#
+# Estimate and Annotation Values
+#
+# Annotation values are character representations of estimates and have values
+# when non-integer information needs to be represented. See the table below for
+# a list of common Estimate/Margin of Error (E/M) values and their corresponding
+# Annotation (EA/MA) values. Please note that ACS data may return the following
+# in place of data.
+#
+# | Estimate Value | Annotation Value | Meaning                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           |
+# | :---           | :---             | :----                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             |
+# | -666666666     | -                | The estimate could not be computed because there were an insufficient number of sample observations. For a ratio of medians estimate, one or both of the median estimates falls in the lowest interval or highest interval of an open-ended distribution. The estimate could not be computed because there were an insufficient number of sample observations. For a ratio of medians estimate, one or both of the median estimates falls in the lowest interval or highest interval of an open-ended distribution. For a 5-year median estimate, the margin of error associated with a median was larger than the median itself. |
+# | -999999999     | N                | The estimate or margin of error cannot be displayed because there were an insufficient number of sample cases in the selected geographic area.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    |
+# | -888888888     | (X)              | The estimate or margin of error is not applicable or not available.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               |
+# | Varies         | median-          | The median falls in the lowest interval of an open-ended distribution (for example "2,500-")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      |
+# | Varies         | median+          | The median falls in the highest interval of an open-ended distribution (for example "250,000+").                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  |
+# | -222222222     | **               | The margin of error could not be computed because there were an insufficient number of sample observations.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       |
+# | -333333333     | ***              | The margin of error could not be computed because the median falls in the lowest interval or highest interval of an open-ended distribution.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      |
+# | -555555555     | *****            | A margin of error is not appropriate because the corresponding estimate is controlled to an independent population or housing estimate. Effectively, the corresponding estimate has no sampling error and the margin of error may be treated as zero.                                                                                                                                                                                                                                                                                                                                                                             |
+# | *              | N/A              | An * indicates that the estimate is significantly different (at a 90% confidence level) than the estimate from the most current year. A "c" indicates the estimates for that year and the current year are both controlled; a statistical test is not appropriate.                                                                                                                                                                                                                                                                                                                                                                |
+# | null           | null             | A null value in the estimate means there is no data available for the requested geography.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        |
+#
+#' @param x a data.frame (data.table) expected to come from importing the data
+#'          via import_census_table.
+#'
+#' @return a list with two elements, the names of the E and M columns with
+#'         annotations.
+check_for_anotations <- function(x) {
+  stopifnot(inherits(x, "data.frame"))
+  E <- endsWith(names(x), "EA")
+  M <- endsWith(names(x), "MA")
+
+  if (!any(E) && !any(M)) {
+    message("No annotation columns")
+  } else {
+    message("annotated columns exist!")
+  }
+  list(E = names(x)[E], M = names(x)[M])
+}
+
+################################################################################
+COLS_TO_KEEP <- c("year", "state", "county", "tract", "block_group")
+
+################################################################################
+#                                 End of File                                  #
+################################################################################
---
+++ ADI/topic01.R
@@ -0,0 +1,102 @@
+################################################################################
+# file: topic01.R
+#
+# Objective: build topic01 of the Area Depredation Index
+#
+#   Topic: 1
+#   Topic Area: % Pop ≥ 25 yrs with < 9 yrs Education
+#   Detailed Table ID: B15003
+#   Calculations:
+#     Numerator: Sum _002 to _012. Denominator: B15003_001
+#
+# Evaluation:
+#
+#   Expect this script to be evaluated with the working directory:
+#   <repo_root>/ADI
+#
+# Notes:
+#
+# | Variable   | Meaning                                    | Part of topic01 |
+# | :--------- | :----------------------------------------- | :-------------: |
+# | B15003_001 | Total                                      | Denominator     |
+# | B15003_002 | No schooling completed                     | Numerator       |
+# | B15003_003 | Nursery school                             | Numerator       |
+# | B15003_004 | Kindergarten                               | Numerator       |
+# | B15003_005 | 1st grade                                  | Numerator       |
+# | B15003_006 | 2nd grade                                  | Numerator       |
+# | B15003_007 | 3rd grade                                  | Numerator       |
+# | B15003_008 | 4th grade                                  | Numerator       |
+# | B15003_009 | 5th grade                                  | Numerator       |
+# | B15003_010 | 6th grade                                  | Numerator       |
+# | B15003_011 | 7th grade                                  | Numerator       |
+# | B15003_012 | 8th grade                                  | Numerator       |
+# | B15003_013 | 9th grade                                  |                 |
+# | B15003_014 | 10th grade                                 |                 |
+# | B15003_015 | 11th grade                                 |                 |
+# | B15003_016 | 12th grade, no diploma                     |                 |
+# | B15003_017 | Regular high school diploma                |                 |
+# | B15003_018 | GED or alternative credential              |                 |
+# | B15003_019 | Some college, less than 1 year             |                 |
+# | B15003_020 | Some college, 1 or more years, no degree   |                 |
+# | B15003_021 | Associate's degree                         |                 |
+# | B15003_022 | Bachelor's degree                          |                 |
+# | B15003_023 | Master's degree                            |                 |
+# | B15003_024 | Professional school degree                 |                 |
+# | B15003_025 | Doctorate degree                           |                 |
+#
+################################################################################
+source("adi_utilities.R")
+
+# import needed data
+#B15003 <- data.table::fread(file = file.path("..", "ACS5", "B15003.csv.gz"))
+DT <- import_census_table(table = "B15003")
+
+check_for_anotations(DT)
+
+# B15003_001MA exists
+# all the annotations are the same:
+stopifnot(
+  DT[!is.na(B15003_001MA), all(B15003_001MA == "*****")]
+)
+# see notes in adi_utilities.R; error can be treated as zero in these cases
+DT[!is.na(B15003_001MA), B15003_001M := 0L]
+
+# We are interested in working on the block_group level for the ADI
+DT <- DT[!is.na(block_group)]
+
+# build the topic
+#
+# if the denominator is zero set the value to NA and denote as QDI-ZD for
+# "Questionable Data Integrity; zero denominator"
+#
+numerator_variables <- sprintf("B15003_%03dE", 2:12)
+
+DT[
+  ,
+  topic01 := data.table::fifelse(B15003_001E > 0, rowSums(.SD, na.rm = TRUE) / B15003_001E, NA_real_),
+  .SDcols = numerator_variables
+  ]
+
+DT[B15003_001E == 0, topic01_suppression := "QDI-ZD"]
+
+# check that all the NA values are accounted for
+stopifnot(
+  DT[topic01_suppression == "QDI-ZD", all(is.na(topic01))],
+  DT[is.na(topic01_suppression), !any(is.na(topic01))]
+)
+
+# save the output to disk
+cols_to_keep <-
+  c(COLS_TO_KEEP,
+    "topic01",
+    "topic01_suppression"
+  )
+
+DT <- DT[, .SD, .SDcols = cols_to_keep]
+data.table::setcolorder(DT, neworder = cols_to_keep)
+data.table::setkeyv(DT, cols = COLS_TO_KEEP)
+data.table::fwrite(DT, file = "topic01.csv")
+
+################################################################################
+#                                 End of File                                  #
+################################################################################
